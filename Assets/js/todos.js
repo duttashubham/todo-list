305442bc3f195c44c2118038747d38f43994338f
duttashubham <58478120+duttashubham@users.noreply.github.com>
2019-12-03 21:33:57 +0530

    Update todos.js

--- Assets/js/todos.js
+++ Assets/js/todos.js
@@ -1,7 +1,10 @@
+//click event on the li
+
 $("ul").on("click", "li", function(){
 $(this).toggleClass("completed");
 });
 
+//click event on the span
 
 $("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
@@ -10,6 +13,8 @@ $("ul").on("click", "span", function(event){
   event.stopPropagation(); 
 });
 
+//keypress event on the input (append())
+
 $("input[type='text']").keypress(function(event){
  if(event.which === 13){
  	var todoText =($(this).val());
@@ -17,6 +22,8 @@ $("input[type='text']").keypress(function(event){
  	$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>");
  }
 });
+
+//fade toggle
 
 $("#nFont").click(function(){
 $("input[type='text']").fadeToggle()
